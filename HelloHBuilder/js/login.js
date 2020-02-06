ee2b4e56f78d969993b0d96812f8c569a0752d65
dugking <bingwei2642@qq.com>
2020-02-06 20:35:18 +0800

    d3 js 学习

---
+++ HelloHBuilder/js/login.js
@@ -0,0 +1,9 @@
+﻿
+$('.login').on('click', function() {
+	var user = $("#user").val().trim();
+	var pwd = $("#pwd").val().trim();
+	if (user == 'root' && pwd == '1512') {
+
+		$(window).attr('location','demo/');
+	}
+});
